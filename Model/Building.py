a9ad41839af775dc5e527450f9b1aeb4b5bfacdd
Le Quoc Dat <quoc_dat.le@insa-cvl.fr>
2021-05-22 20:04:03 +0200

    Building + Unit Class

---
+++ Model/Building.py
@@ -0,0 +1,40 @@
+class Building:
+
+    def __init__(self, pos, building_type):
+        self.pos = pos 
+        self.building_type = building_type
+        if building_type == "S":
+            self.can_attack = False
+            self.price = None
+            self.life = 240
+            self.score_killed = 100
+        elif building_type == "C":
+            self.can_attack = False
+            self.price = 250
+            self.life = 160
+            self.score_killed = 60
+        elif building_type == "T":
+            self.can_attack = True
+            self.price = 70
+            self.attack = 35
+            self.life = 60
+            self.attack_distance = 2
+            self.score_killed = 30
+        elif building_type == "W":
+            self.can_attack = False
+            self.price = 30
+            self.life = 120
+            self.score_killed = 15
+        else: 
+            assert("Error : Wrong building type!")
+
+    def __repr__(self):
+        return "type {0}; life {1}; price{2}".format(self.type, self.life, self.price)
+
+    def __str__(self):
+        return "type {0}; life {1}; price{2}".format(self.type, self.life, self.price)
+
+
+
+
+
---
+++ Model/Unit.py
@@ -0,0 +1,38 @@
+class Unit:
+
+    def __init__(self, pos_spawn_building, pos, unit_type):
+        self.pos_building_spawn = pos_building_spawn
+        self.unit_type = unit_type
+        self.pos = pos
+        if unite_type == "V":
+            self.price = 20
+            self.life = 20
+            self.attack = 10
+            self.movement = 2
+            self.score_killed = 10
+            self.capability = "Construction de batiment, recolte de ressources"
+            self.spawn_building = "S"
+        elif unit_type == "L":
+            self.price = 30
+            self.life = 60
+            self.attack = 20
+            self.movement = 4
+            self.score_killed = 15
+            self.capability = None
+            self.spawn_building = "Amphitheatre"
+        else:
+            self.price = 100
+            self.life = 100
+            self.attack = 40
+            self.movement = 2
+            self.score_killed = 35
+            self.capability = None
+            self.spawn_building = "Amphitheatre"
+
+    def __str__(self):
+        return "Type:{0}; Cout:{1}; Point de vie: {2}; Point attaque : {3}; Point de mouvement: {4}; Point gagne si tue : {5}; Capacite: {6}; Batiment de formation : {7};Position de batiment attitre: {8} ".format(self.unit_type,self.price, self.life, self.attack, self.movement, self.score_killed, self.capability, self.spawn_building, self.pos_building_spawn);
+
+    def __repr__(self):
+        return "Type:{0}; Cout:{1}; Point de vie: {2}; Point attaque : {3}; Point de mouvement: {4}; Point gagne si tue : {5}; Capacite: {6}; Batiment de formation : {7};Position de batiment attitre: {8} ".format(self.unit_type,self.price, self.life, self.attack, self.movement, self.score_killed, self.capability, self.spawn_building, self.pos_building_spawn);
+
+
