b77150cebbf59cff13f1fdfb893cc66570ec2c2f
Le Quoc Dat <quoc_dat.le@insa-cvl.fr>
2021-05-22 21:49:31 +0200

    New grid

--- Model/Map.py
+++ Model/Map.py
@@ -4,8 +4,27 @@ class Map:
 
     def __init__(self, json_map):
         rows = (json_map.splitlines())
-        self.grid = [[Case(info) for info in row.split(" ")] for row in rows]
-        print(self.grid)
+        self.height = len(rows)
+        self.grid = []
+        for row in rows:
+            row = row.split(" ")
+            self.width = len(row)
+            y_dimension = []
+            i = 0
+            while i<len(row):
+                z_dimension = []
+                z_dimension.append(row[i])
+                z_dimension.append(row[i+1])
+                i += 2
+                y_dimension.append(z_dimension)
+            self.grid.append(y_dimension)
+        print("test map : height + width == ", self.height, self.width)
+        for x in self.grid:
+            for y in x:
+                for z in y:
+                    print(z, end="/") 
+                print(" ", end="")
+            print()
 
     def __repr__(self):
         [print(''.join(str(ligne)).strip('[]')) for ligne in self.grid]
