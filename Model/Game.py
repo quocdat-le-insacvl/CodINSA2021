bd4ff4d334053279df68dd2672a705686dd70660
Arnaud <arrnicel@gmail.com>
2021-05-23 12:22:18 +0200

    Add montagne déplacement

--- Model/Game.py
+++ Model/Game.py
@@ -101,10 +101,11 @@ class Game:
         posSpawnEnemie = find_enemy_spawn(self.map)
         for unit in self.map.list_unit:
             list_Path = self.map.pathFinder(tuple(unit.pos), adjPos(posSpawnEnemie)[random.randint(0,2)])
-            print("deplacement", unit.pos, list_Path)
             if list_Path is not None:
-                print("deplacement", unit.pos, list_Path)
-                turn.move(unit.pos, list_Path[0:1])
+                if self.map.grid[list_Path[0][1]][list_Path[0][0]][list_Path[0][2]].tiles_type=="M":
+                    turn.move(unit.pos, list_Path[0:(unit.movement//2)])
+                else:
+                    turn.move(unit.pos, list_Path[0:unit.movement])
                 unit.action_attack()
                 unit.build()
                 unit.dig()
